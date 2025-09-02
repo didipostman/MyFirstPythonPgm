172a838b8bec60e7d92fa723292eb5716e9f9a12
Wadï Mami <88431402+didipostman@users.noreply.github.com>
2025-09-02 03:53:52 +0100

    Update FirstPgm.py
    Verify entries

--- FirstPgm.py
+++ FirstPgm.py
@@ -1,24 +1,35 @@
 import random
 number_to_guess = random.randint(1, 10)
 
-# Prompt the user to enter an integer between 1 et 10
-user_input_str = input("Please enter a whole number between 1 et 10:\n ")
-# Convert the input string to an integer
-number_as_int = int(user_input_str)
 # number of attempts
-number_of_attempt = 1
-  
-while number_as_int != number_to_guess :
+number_of_attempt = 0
+begin = False
+
+input_str = input("Please enter a whole number between 1 et 10:\n ")
+while not begin :
+
+
+     
+    while (input_str.isdigit() == False) :
+        input_str = input("Please enter a whole number between 1 et 10:\n ")
         
+ 
+    while int(input_str) not in range (1,11) :
+        input_str = input("Please enter a whole number between 1 et 10:\n ")
+        
+    number_as_int = int(input_str)
+
+    
     if number_as_int > number_to_guess:
         print("number you give > number to guess ")
+        input_str = input("Please enter a whole number between 1 et 10:\n ")
         
     elif  number_as_int < number_to_guess:
         print("number you give < number to guess ")
-        
-
-    user_input_str = input("Please enter a whole number between 1 et 10:\n ")
-    number_as_int = int(user_input_str)
+        input_str = input("Please enter a whole number between 1 et 10:\n ")
+    if number_as_int == number_to_guess :
+      begin = True
+      
     number_of_attempt = number_of_attempt + 1
 
 
